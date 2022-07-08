0617aae5352e8cb0a234ed1d55bae37ba1cfc3c9
gongchengzhi <gongcz@koal.com>
2022-07-08 09:46:06 +0800

    ipfs file write support

--- example/ipfs/main.go
+++ example/ipfs/main.go
@@ -12,19 +12,23 @@ func main() {
 	var files []*ipfs.File
 	var fstat *ipfs.FileStat
 
+	//create dir
 	err = ipfs.IpfsCreateDir("/test/test", true)
 	if err != nil {
 		panic(err)
 	}
+	//file stat example
 	if fstat, err = ipfs.IpfsFileStat("/test/test"); err != nil {
 		panic(err)
 	}
 	fmt.Println("test stat:", fstat)
+	//file remove example
 	err = ipfs.IpfsFileRemove("/test/test", true, true)
 	if err != nil {
 		panic(err)
 	}
 
+	//file list example
 	files, err = ipfs.IpfsFileList("/")
 	if err != nil {
 		panic(err)
@@ -32,11 +36,13 @@ func main() {
 	fmt.Println(files)
 	var fn = "/test_golang.txt"
 	var fpath = ipfs.NewFileWriteOptions(fn)
+	//file write example
 	if num, err = ipfs.IpfsFileWrite(fpath, []byte("12345678910ABC")); err != nil {
 		panic(err)
 	}
 	fmt.Println("file write", fn, num)
 	var buf = make([]byte, 1024)
+	//file read example
 	if num, err = ipfs.IpfsFileRead(fn, 0, buf); err != nil {
 		panic(err)
 	}
